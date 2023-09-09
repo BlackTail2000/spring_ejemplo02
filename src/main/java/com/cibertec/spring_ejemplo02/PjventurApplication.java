dcd8272b6ce309de8bcb1bbb32b695a96cbe3e41
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2023-09-08 22:49:04 -0500

    Proyecto creado

---
+++ src/main/java/com/cibertec/spring_ejemplo02/PjventurApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.spring_ejemplo02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjventurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjventurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/spring_ejemplo02/PjventurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.spring_ejemplo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjventurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
